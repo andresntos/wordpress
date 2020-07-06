e3bda3e68b4738506597c2c327514d6637025dd5
andresntos <57955463+andresntos@users.noreply.github.com>
2020-07-06 02:29:03 -0300

    Create as_excerpt.php

---
+++ as_excerpt.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  This function returns a Wordpress title tag.
+ *  - Put the function as_excerpt(){...} in your functions.php theme file;
+ *  - Call the function as_excerpt(); in the category.php, search.php or where you wanto to show the text.
+ * 
+ * @param integer $words: Total of words to show in output text.
+ * @return void
+ */
+
+function as_excerpt($words = 40){
+    global $post;
+    $strAllowedTags = '<i>,<em>,<b>,<strong>,<ul>,<ol>,<li>,<span>';
+    $strText = explode( ' ', preg_replace('/\[.*\]/', '', strip_tags($post->post_content, $strAllowedTags) ) );
+    $intTot = count($strText);
+    
+    for($i = 0; $i < $words; $i++){
+        $strOutput .= $strText[$i] . ' '; 
+    }
+
+    $strOutput = convert_smilies($strOutput);
+
+    echo '<p>',
+    force_balance_tags($strOutput),
+    $i < $intTot ? '...<p>' : '<p>',
+    '<a href="' . get_permalink() . '" title="'.get_the_title().'">Continue lendo</a></p>';
+}
