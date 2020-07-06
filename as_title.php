958753f4d7cc9ba0458bb6307459f493038c8bb2
andresntos <57955463+andresntos@users.noreply.github.com>
2020-07-06 02:30:18 -0300

    Update as_title.php

--- as_title.php
+++ as_title.php
@@ -9,22 +9,22 @@
  */
 
 function as_title(){
-    if (is_home()){
+    if ( is_home() ){
         return get_bloginfo('name') . ' | ' . get_bloginfo('description');
-    } elseif (is_page() || is_single()){
+    }elseif( is_page() || is_single() ){
         return get_the_title() . ' | ' . get_bloginfo('name');
-    } elseif (is_category()){
+    }elseif( is_category() ){
         return 'Category: ' . single_cat_title('',false) . ' | ' . get_bloginfo('name');
-    } elseif (is_month()){
+    }elseif( is_month() ){
         return 'Archive: ' . get_the_time('F') . ' de ' . get_the_time('Y') . ' | ' . get_bloginfo('name'); 
-    } elseif (is_year()) {
+    }elseif( is_year() ) {
         return 'Archive: ' . get_the_time('Y') . ' | ' . get_bloginfo('name');
-    } elseif (is_tag() || is_author() ) {
+    }elseif( is_tag() || is_author() ) {
         return 'Archive: ' . single_cat_title('',false) . ' | ' . get_bloginfo('name');
-    } elseif (is_search()){
+    }elseif( is_search() ){
         global $s;
         return 'Search: ' . $s . ' | ' . get_bloginfo('name');
-    } elseif (is_404()){
+    }elseif( is_404() ){
         return '404 - Ops! Page not found. | ' . get_bloginfo('name');
     }
     return get_bloginfo('name') . ' | ' . get_bloginfo('description');
